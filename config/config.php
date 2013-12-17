450cd7bf4c51173ec13b97a84cd74ae61a2c3f5b
cmb69 <cmbecker69@gmx.de>
2013-12-17 22:06:52 +0000

    changed default license to GPLv3

--- config/config.php
+++ config/config.php
@@ -7,7 +7,7 @@ $plugin_cf['translator']['translate_fullscreen']="1";
 $plugin_cf['translator']['folder_download']="downloads/";
 
 $plugin_cf['translator']['translation_author']="";
-$plugin_cf['translator']['translation_license']="This work is licensed under a Creative Commons Attribution 3.0 Unported License.";
+$plugin_cf['translator']['translation_license']="This work is licensed under the GNU General Public License v3.";
 
 $plugin_cf['translator']['sort_load']="1";
 $plugin_cf['translator']['sort_save']="0";
