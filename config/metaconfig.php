25bd8f7805341e8a003ae4a93ac5004b9d9c732c
cmb69 <cmbecker69@gmx.de>
2013-12-19 22:07:14 +0000

    added metaconfig.php

---
+++ config/metaconfig.php
@@ -0,0 +1,10 @@
+<?php
+
+$plugin_mcf['translator']['translate_from']="function:XH_availableLocalizations";
+$plugin_mcf['translator']['translate_to']="function:XH_availableLocalizations";
+$plugin_mcf['translator']['translate_fullscreen']="bool";
+$plugin_mcf['translator']['translation_license']="text";
+$plugin_mcf['translator']['sort_load']="bool";
+$plugin_mcf['translator']['sort_save']="bool";
+
+?>
