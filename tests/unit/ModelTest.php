d631e8285c5054ab1f8744182c5b785de3dbb743
cmb69 <cmbecker69@gmx.de>
2013-12-19 12:57:19 +0000

    added unit tests

--- tests/unit/ModelTest.php
+++ tests/unit/ModelTest.php
@@ -10,18 +10,82 @@ class ModelTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        global $pth;
+        global $pth, $tx;
 
         vfsStreamWrapper::register();
-        vfsStreamWrapper::setRoot(new vfsStreamDirectory(''));
-        $this->basePath = vfsStream::url('');
+        vfsStreamWrapper::setRoot(new vfsStreamDirectory('test'));
+        $this->basePath = vfsStream::url('test') . '/';
         $pth = array(
             'folder' => array(
+                'base' => $this->basePath,
+                'downloads' => $this->basePath . 'downloads/',
+                'flags' => $this->basePath . 'images/flags/',
                 'language' => $this->basePath . 'cmsimple/languages/',
                 'plugins' => $this->basePath . 'plugins/'
             )
         );
+        mkdir($pth['folder']['flags'], 0777, true);
+        file_put_contents($pth['folder']['flags'] . 'en.gif', '');
+        mkdir($pth['folder']['language'], 0777, true);
+        mkdir($pth['folder']['plugins'], 0777, true);
+        mkdir($pth['folder']['plugins'] . 'pluginloader/languages', 0777, true);
+        mkdir($pth['folder']['plugins'] . 'foo/languages', 0777, true);
+        mkdir($pth['folder']['plugins'] . 'bar/languages', 0777, true);
+        $this->texts = array(
+            'a' => 'one',
+            'b' => 'two'
+        );
+        $tx = array(
+            'error' => array(
+                'cntopen' => '', 'cntwriteto' => '', 'notreadable' => ''
+            )
+        );
         $this->model = new Translator_Model();
+    }
+
+    public function testCanonicalUrl()
+    {
+        $url = 'http://example.com/./foo/bar/../baz/./index.html';
+        $expected = 'http://example.com/foo/baz/index.html';
+        $actual = $this->model->canonicalUrl($url);
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testFlagIconPath()
+    {
+        $expected = $this->basePath . 'images/flags/en.gif';
+        $actual = $this->model->flagIconPath('en');
+        $this->assertEquals($expected, $actual);
+
+        $actual = $this->model->flagIconPath('de');
+        $this->assertFalse($actual);
+    }
+
+    public function testDownloadFolder()
+    {
+        global $pth;
+
+        $expected = $this->basePath . 'downloads/';
+        $actual = $this->model->downloadFolder();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testPlugins()
+    {
+        global $pth;
+
+        $expected = array('bar', 'foo', 'pluginloader');
+        $actual = $this->model->plugins();
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testModules()
+    {
+        global $pth;
+
+        $expected = array('CORE', 'CORE-LANGCONFIG', 'bar', 'foo', 'pluginloader');
+        $actual = $this->model->modules();
+        $this->assertEquals($expected, $actual);
     }
 
     public function dataForFilename()
@@ -43,6 +107,48 @@ class ModelTest extends PHPUnit_Framework_TestCase
         $expected = $this->basePath . $expected;
         $actual = $this->model->filename($module, $language);
         $this->assertEquals($expected, $actual);
+    }
+
+    public function dataForWritingAndReading()
+    {
+        return array(
+            array(
+                'CORE', 'en',
+                array(
+                    'a_1' => 'one', 'a_2' => 'two',
+                    'b_1' => 'three', 'b_2' => 'four'
+                )
+            ),
+            array(
+                'CORE-LANGCONFIG', 'en',
+                array(
+                    'a_1' => 'one', 'a_2' => 'two',
+                    'b_1' => 'three', 'b_2' => 'four'
+                )
+            ),
+            array(
+                'pluginloader', 'en',
+                array(
+                    'a_1' => 'one', 'a_2' => 'two',
+                    'b_1' => 'three', 'b_2' => 'four'
+                )
+            ),
+            array(
+                'foo', 'en',
+                array('a' => 'one', 'b' => 'two')
+            )
+        );
+    }
+
+    /**
+     * @dataProvider dataForWritingAndReading
+     */
+    public function testWritingAndReading($module, $language, $array)
+    {
+        $success = $this->model->writeLanguage($module, $language, $array);
+        $this->assertTrue($success);
+        $texts = $this->model->readLanguage($module, $language);
+        $this->assertEquals($array, $texts);
     }
 }
 
---
+++ tests/unit/ViewsTest.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once './classes/Model.php';
+require_once './classes/Views.php';
+
+define('TRANSLATOR_VERSION', 'test');
+
+function tag($string)
+{
+    return $string;
+}
+
+class ViewsTest extends PHPUnit_Framework_TestCase
+{
+    protected $views;
+
+    public function setUp()
+    {
+        global $cf/*, $plugin_tx*/;
+
+        $cf = array('xhtml' => array('endtags' => ''));
+        //$plugin_tx = array('translator' => array('label_download_url' => ''));
+        $model = $this->getMockBuilder('Translator_Model')
+                      ->disableOriginalConstructor()
+                      ->getMock();
+        $model->expects($this->any())
+              ->method('modules')
+              ->will($this->returnValue(array('foo')));
+        $model->expects($this->any())
+              ->method('readLanguage')
+              ->will($this->returnValue(array('a' => 'one')));
+        $this->views = new Translator_Views($model);
+    }
+
+    public function testAboutShowsVersionInfo()
+    {
+        $matcher = array('tag' => 'p', 'content' => 'Version: test');
+        $actual = $this->views->about('');
+        $this->assertTag($matcher, $actual);
+    }
+
+    public function testAboutShowsPluginIcon()
+    {
+        $icon = 'icon.png';
+        $matcher = array('tag' => 'img', 'attributes' => array('src' => $icon));
+        $actual = $this->views->about($icon);
+        $this->assertTag($matcher, $actual);
+    }
+
+    public function testSystemCheck()
+    {
+        $checks = array('everything' => 'ok');
+        $matcher = array(
+            'tag' => 'ul',
+            'children' => array(
+                'count' => 1,
+                'only' => array('tag' => 'li')
+            )
+        );
+        $actual = $this->views->systemCheck($checks);
+        $this->assertTag($matcher, $actual);
+    }
+
+    public function testMain()
+    {
+        $matcher = array('tag' => 'form');
+        $actual = $this->views->main('', '', '', array());
+        $this->assertTag($matcher, $actual);
+    }
+
+    public function testEditor()
+    {
+        $matcher = array('tag' => 'form');
+        $actual = $this->views->editor('', 'foo', 'en', 'de');
+        $this->assertTag($matcher, $actual);
+    }
+
+    public function testDownloadUrl()
+    {
+        $matcher = array('tag' => 'input');
+        $actual = $this->views->downloadUrl('');
+        $this->assertTag($matcher, $actual);
+    }
+}
+
+?>
