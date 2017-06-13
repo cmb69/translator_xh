cmb69/translator_xh

commit e6bd1c4d40396a11c475214db7435b98c056cc27

Namespace classes

classes/Model.php
@@ -14,6 +14,8 @@
  * @link      http://3-magi.net/?CMSimple_XH/Translator_XH
  */
 
+namespace Translator;
+
 /**
  * The model class.
  *
@@ -23,7 +25,7 @@
  * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link     http://3-magi.net/?CMSimple_XH/Translator_XH
  */
-class Translator_Model
+class Model
 {
     /**
      * The names of special modules.

admin.php
@@ -27,6 +27,6 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
  */
 define('TRANSLATOR_VERSION', '@TRANSLATOR_VERSION@');
 
-new Translator_Controller();
+new Translator\Controller();
 
 ?>

classes/Controller.php
@@ -14,6 +14,8 @@
  * @link      http://3-magi.net/?CMSimple_XH/Translator_XH
  */
 
+namespace Translator;
+
 /**
  * The controller class.
  *
@@ -23,19 +25,19 @@
  * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link     http://3-magi.net/?CMSimple_XH/Translator_XH
  */
-class Translator_Controller
+class Controller
 {
     /**
      * The translator model.
      *
-     * @var Translator_Model
+     * @var Model
      */
     protected $model;
 
     /**
      * The translator views.
      *
-     * @var Translator_Views
+     * @var Views
      */
     protected $views;
 
@@ -46,8 +48,8 @@ class Translator_Controller
      */
     public function __construct()
     {
-        $this->model = new Translator_Model();
-        $this->views = new Translator_Views($this->model);
+        $this->model = new Model();
+        $this->views = new Views($this->model);
         $this->dispatch();
     }
 

classes/Views.php
@@ -14,6 +14,8 @@
  * @link      http://3-magi.net/?CMSimple_XH/Translator_XH
  */
 
+namespace Translator;
+
 /**
  * The views class.
  *
@@ -23,21 +25,21 @@
  * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
  * @link     http://3-magi.net/?CMSimple_XH/Translator_XH
  */
-class Translator_Views
+class Views
 {
     /**
      * The translator model.
      *
-     * @var Translator_Model
+     * @var Model
      */
     protected $model;
 
     /**
      * Initializes a new instance.
      *
-     * @param Translator_Model $model A translator model.
+     * @param Model $model A translator model.
      */
-    public function __construct(Translator_Model $model)
+    public function __construct(Model $model)
     {
         $this->model = $model;
     }

classes/required_classes.php
@@ -20,7 +20,7 @@
  */
 
 spl_autoload_register(function ($className) {
-    $parts = explode('_', $className);
+    $parts = explode('\\', $className);
     if ($parts[0] == 'Translator') {
         include_once __DIR__ . '/' . $parts[1] . '.php';
     }

tests/bootstrap.php
@@ -3,3 +3,8 @@
 require_once './vendor/autoload.php';
 
 require_once './classes/required_classes.php';
+
+function tag($string)
+{
+    return $string;
+}

tests/unit/ModelTest.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Translator;
+
+use PHPUnit_Framework_TestCase;
 use org\bovigo\vfs\vfsStreamWrapper;
 use org\bovigo\vfs\vfsStreamDirectory;
 use org\bovigo\vfs\vfsStream;
@@ -42,7 +45,7 @@ class ModelTest extends PHPUnit_Framework_TestCase
                 'cntopen' => '', 'cntwriteto' => '', 'notreadable' => ''
             )
         );
-        $this->model = new Translator_Model();
+        $this->model = new Model();
     }
 
     public function testCanonicalUrl()

tests/unit/ViewsTest.php
@@ -1,11 +1,10 @@
 <?php
 
-define('TRANSLATOR_VERSION', 'test');
+namespace Translator;
 
-function tag($string)
-{
-    return $string;
-}
+use PHPUnit_Framework_TestCase;
+
+define('TRANSLATOR_VERSION', 'test');
 
 class ViewsTest extends PHPUnit_Framework_TestCase
 {
@@ -17,7 +16,7 @@ class ViewsTest extends PHPUnit_Framework_TestCase
 
         $cf = array('xhtml' => array('endtags' => ''));
         //$plugin_tx = array('translator' => array('label_download_url' => ''));
-        $model = $this->getMockBuilder('Translator_Model')
+        $model = $this->getMockBuilder('Translator\Model')
                       ->disableOriginalConstructor()
                       ->getMock();
         $model->expects($this->any())
@@ -26,7 +25,7 @@ class ViewsTest extends PHPUnit_Framework_TestCase
         $model->expects($this->any())
               ->method('readLanguage')
               ->will($this->returnValue(array('a' => 'one')));
-        $this->views = new Translator_Views($model);
+        $this->views = new Views($model);
     }
 
     public function dataForMessage()

commit 779aa8227a378f73c4993aee032c106f2c4a273e

Fix regression introduced in e6bd1c4
Class `zipfile` is outside the `Translator` namespace.

classes/Model.php
@@ -21,6 +21,8 @@
 
 namespace Translator;
 
+use zipfile;
+
 class Model
 {
     /**