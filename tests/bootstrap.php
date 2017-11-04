1b7694fec616d4d004419c0e2f9af8c70adf51b2
Christoph M. Becker <cmbecker69@gmx.de>
2017-11-04 19:14:44 +0100

    Use Pfw_XH's class autoloader

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,4 +4,4 @@ require_once './vendor/autoload.php';
 
 require_once '../../cmsimple/functions.php';
 
-require_once './classes/required_classes.php';
+require_once '../pfw/classes/required_classes.php';
