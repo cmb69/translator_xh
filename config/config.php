bd1fe7fd35fef7cc0f6b1b1601ebf069b451220b
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-08 17:02:38 +0200

    Restore inadvertently changed config/language

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 $plugin_cf['translator']['translate_from'] = "en";
 $plugin_cf['translator']['translate_to'] = "de";
 $plugin_cf['translator']['translate_fullscreen'] = "";
-$plugin_cf['translator']['translation_author'] = "Christoph M. Becker";
-$plugin_cf['translator']['translation_license'] = "This work is licensed under the GNU General Public License v3. Blah blah blah blah blah.";
+$plugin_cf['translator']['translation_author'] = "";
+$plugin_cf['translator']['translation_license'] = "This work is licensed under the GNU General Public License v3.";
 $plugin_cf['translator']['sort_load'] = "1";
 $plugin_cf['translator']['sort_save'] = "0";
