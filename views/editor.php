e41215da62d528477f620e0247ae47a601d9028c
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-09 00:02:15 +0200

    Lay out editor as grid instead of table
    That makes it easier to customize.

--- views/editor.php
+++ views/editor.php
@@ -16,24 +16,20 @@ use Plib\View;
 <form id="translator" method="post" action="<?=$this->esc($action)?>">
   <h1>Translator – <?=$moduleName?></h1>
   <input type="submit" class="submit" value="<?=$this->text('label_save')?>">
-  <table>
-    <tr>
-      <th></th>
-      <th><?=$this->text('label_translate_from')?> <?=$this->raw($sourceLabel)?></th>
-      <th><?=$this->text('label_translate_to')?> <?=$this->raw($destinationLabel)?></th>
-    </tr>
+  <div>
+    <p></p>
+    <p><?=$this->text('label_translate_from')?> <?=$this->raw($sourceLabel)?></p>
+    <p><?=$this->text('label_translate_to')?> <?=$this->raw($destinationLabel)?></p>
 <?php foreach ($rows as $row):?>
-    <tr>
-      <td class="translator_key"><?=$this->esc($row->displayKey)?></td>
-      <td class="translator_from">
-        <textarea rows="2" cols="40" readonly="readonly"><?=$this->esc($row->sourceText)?></textarea>
-      </td>
-      <td class="translator_to">
-        <textarea name="translator_string_<?=$this->esc($row->key)?>" class="<?=$this->esc($row->className)?>" rows="2" cols="40"><?=$this->esc($row->destinationText)?></textarea>
-      </td>
-    </tr>
+    <div class="translator_key"><?=$this->esc($row->displayKey)?></div>
+    <div class="translator_from">
+      <textarea rows="2" cols="40" readonly="readonly"><?=$this->esc($row->sourceText)?></textarea>
+    </div>
+    <div class="translator_to">
+      <textarea name="translator_string_<?=$this->esc($row->key)?>" class="<?=$this->esc($row->className)?>" rows="2" cols="40"><?=$this->esc($row->destinationText)?></textarea>
+    </div>
 <?php endforeach?>
-  </table>
+  </div>
   <input type="submit" class="submit" value="<?=$this->text('label_save')?>">
   <input type="hidden" name="translator_token" value="<?=$this->esc($csrf_token)?>">
 </form>
