e1de65619f37783fa47fcebb5338f671063de693
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-09 15:10:12 +0200

    Drop irrelevant info from plugin info screen
    Copyright and license are already show in the user manual, and this is
    nothing users need to see all the time.

--- views/info.php
+++ views/info.php
@@ -4,37 +4,12 @@ use Plib\View;
 
 /**
  * @var View $this
- * @var string $logo
  * @var string $version
  * @var list<object{state:string,label:string,stateLabel:string}> $checks
  */
 ?>
 
-<h1>Translator</h1>
-<img src="<?=$this->esc($logo)?>" alt="<?=$this->text('alt_logo')?>" class="pfw_logo">
-<p>
-  Version: <?=$this->esc($version)?>
-</p>
-<p>
-  Copyright © 2011-2017 <a href="http://3-magi.net/">Christoph M. Becker</a>
-</p>
-<p class="pfw_license">
-  Translator_XH is free software: you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published by the Free
-  Software Foundation, either version 3 of the License, or (at your option)
-  any later version.
-</p>
-<p class="pfw_license">
-  Translator_XH is distributed in the hope that it will be useful, but
-  <em>without any warranty</em>; without even the implied warranty of
-  <em>merchantability</em> or <em>fitness for a particular purpose</em>. See
-  the GNU General Public License for more details.
-</p>
-<p class="pfw_license">
-  You should have received a copy of the GNU General Public License along with
-  Translator_XH. If not, see <a
-  href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
-</p>
+<h1>Translator <?=$this->esc($version)?></h1>
 <div class="pfw_syscheck">
   <h2><?=$this->text('syscheck_title')?></h2>
 <?foreach ($checks as $check):?>
